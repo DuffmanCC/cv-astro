fad15b2266fbe6a8c96aad0d77af36682b46ac50
Carlos Ortiz <carlosor@gmail.com>
2025-05-21 18:58:57 +0200

    add build:with-pdf script

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import pdf from "astro-pdf";
 import { defineConfig } from "astro/config";
 import path from "path";
 
-// https://astro.build/config
+const isCI = process.env.CI === "true"; // gha define esta variable
+
 export default defineConfig({
   site: "https://www.carlosortiz.dev",
   vite: {
@@ -16,15 +17,14 @@ export default defineConfig({
     },
   },
   integrations: [
-    pdf({
-      pages: {
-        "/print": {
-          path: "./carlos-ortiz-resume.pdf",
-          pdf: {
-            format: "A4",
+    !isCI &&
+      pdf({
+        pages: {
+          "/print": {
+            path: "./carlos-ortiz-resume.pdf",
+            pdf: { format: "A4" },
           },
         },
-      },
-    }),
-  ],
+      }),
+  ].filter(Boolean),
 });
