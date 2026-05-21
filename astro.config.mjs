b142a18c1379724b66cc2bfb7904bbc66bd95770
Carlos Ortiz <carlosor@gmail.com>
2026-05-21 11:57:30 +0200

    fix: update GitHub Actions workflow and improve PDF integration in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import pdf from "astro-pdf";
 import { defineConfig } from "astro/config";
 import path from "path";
 
-const isCI = process.env.CI === "true"; // gha define esta variable
-
 export default defineConfig({
   site: "https://www.carlosortiz.dev",
   vite: {
@@ -17,14 +15,13 @@ export default defineConfig({
     },
   },
   integrations: [
-    !isCI &&
-      pdf({
-        pages: {
-          "/print": {
-            path: `./carlos-ortiz-resume.pdf`,
-            pdf: { format: "A4" },
-          },
+    pdf({
+      pages: {
+        "/print": {
+          path: `./carlos-ortiz-resume.pdf`,
+          pdf: { format: "A4" },
         },
-      }),
+      },
+    }),
   ].filter(Boolean),
 });
